b98b6927c0167a8b72cd643ba1271955e921bf0b
williamcwi <williamcwi.chu@hotmail.com>
2025-02-23 15:21:53 +0000

    ReactFacts

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/src/assets/react.svg" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
